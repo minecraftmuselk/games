df967e1efc650fcb6128e37a231ff998cb387869
Finan Illikkal <finani007@gmail.com>
2024-07-01 13:03:26 -0400

    jk

---
+++ guess_the_word.py
@@ -0,0 +1,32 @@
+secret_word = 'illikkal'
+dashes = '-' * len(secret_word)
+
+
+def update_dashes():
+    empty = ''
+    for i in range(len(secret_word)):
+
+
+def get_guess():
+    for i in range(10):
+        print(dashes)
+        guess = input('Guess: ')
+
+        if guess in secret_word:
+            print('That letter is in the secret word!')
+        elif guess not in secret_word:
+            print('That letter is not in the secret word!')
+
+            if guess.isupper():
+                print('Your guess must be a lowercase letter!')
+            if guess.isdigit():
+                print('Your guess cannot be a number!')
+            if len(guess) > 1:
+                print('Your guess must have exactly one character!')
+
+
+def main():
+    get_guess()
+
+
+main()
